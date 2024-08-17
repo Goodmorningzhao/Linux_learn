8a6328e3ed56d60e9b5d09d85d92fe9261cf9c3d
Goodmorningzxp <Goodmorningmm@msn.cn>
2024-08-17 19:09:55 +0800

    Firstly inital the project start kernel on sd without u-boot

--- start_kernel.sh
+++ start_kernel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-qemu-system-arm \
-    -M vexpress-a9 \
-    -m 512M \
-    -nographic \
-    -kernel project_kernel/arch/arm/boot/zImage \
-    -dtb project_kernel/arch/arm/boot/dts/vexpress-v2p-ca9.dtb \
-    -append "root=/dev/mmcblk0 rw console=ttyAMA0" \
-    -sd project_rootfs/vexpress.img
+sudo qemu-system-arm \
+        -M vexpress-a9 \
+        -m 512M \
+        -nographic \
+        -kernel project_kernel/arch/arm/boot/zImage \
+        -dtb project_kernel/arch/arm/boot/dts/vexpress-v2p-ca9.dtb \
+        -append "root=/dev/mmcblk0 rw console=ttyAMA0" \
+        -sd project_rootfs/vexpress-ca9.img
 
--- start_uboot.sh
+++ start_uboot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-qemu-system-arm \
-    -M vexpress-a9 \
-    -nographic \
-    -m 512M \
-    -kernel u-boot
+sudo qemu-system-arm \
+        -M vexpress-a9 \
+        -nographic \
+        -m 512M \
+        -kernel project_uboot/u-boot
